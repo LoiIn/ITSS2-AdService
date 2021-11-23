edbd20a12fa38840deb9971acc9b689e8aa99474
phu-bui <hunter17599@gmail.com>
2021-11-24 01:29:48 +0700

    refactor

--- routes/store.php
+++ routes/store.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\Web\Auth\LoginController;
-use App\Http\Controllers\Web\HomeController;
+use App\Http\Controllers\Store\Auth\LoginController;
+use App\Http\Controllers\Store\HomeController;
+use App\Http\Controllers\Report\ReportController;
 use Illuminate\Support\Facades\Route;
 
 Route::match(['get', 'post'], '/login', [LoginController::class, 'login'])->name('store.login');
