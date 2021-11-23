ab98c0cb3798f40253183785e58d2360f2d80369
phu-bui <hunter17599@gmail.com>
2021-11-24 01:18:00 +0700

    Add login

--- routes/store.php
+++ routes/store.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Http\Controllers\Store\Auth\LoginController;
-use App\Http\Controllers\Store\HomeController;
+use App\Http\Controllers\Web\Auth\LoginController;
+use App\Http\Controllers\Web\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::match(['get', 'post'], '/login', [LoginController::class, 'login'])->name('store.login');
-Route::get('/logout', [LoginController::class, 'logout']);
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 Route::middleware('auth:store')->group(function (){
     Route::get('/', [HomeController::class, 'index'])->name('dashboard');
 });
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,19 @@
 <?php
 
-use App\Http\Controllers\User\Auth\LoginController;
-use App\Http\Controllers\User\HomeController;
+use App\Http\Controllers\Web\Auth\LoginController;
+use App\Http\Controllers\Web\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Advertisement\AdvertisementController;
 
-Route::match(['get', 'post'], '/login', [LoginController::class, 'login'])->name('login');
-Route::middleware('auth')->group(function (){
-    Route::get('/', [HomeController::class, 'index'])->name('home');
+//Route::group(['prefix' => '', 'namespace' => 'App\Http\Controllers\Web\HomeController'], function() {
+//    Route::get('/', 'HomeController@index')->name('web.home');
+//};
+
+
+Route::middleware('auth:store')->group(function (){
+    Route::get('/', [HomeController::class, 'index'])->name('web.home');
+    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('web.login');
+    Route::post('/login', [LoginController::class, 'signUp'])->name('web.post-login');
+    Route::post('/register', [LoginController::class, 'signUp'])->name('web.post-register');
+    Route::get('/logout', [LoginController::class, 'logout'])->name('web.logout');
 });
-
-Route::prefix('advertisement')->group(function(){
-    Route::get('/', [AdvertisementController::class, 'index'])->name('advertisement.index');
-    Route::get('destroy/{id}', [AdvertisementController::class, 'destroy'])->name('advertisement.destroy');
-   
-});
-
-
