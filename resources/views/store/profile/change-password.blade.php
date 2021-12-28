4affce3052b55f80a2c962c5aa3f5cc4a560659d
anh nguyen <anhmeorv99@gmail.com>
2021-12-28 15:13:37 +0700

    fix form

--- resources/views/store/profile/change-password.blade.php
+++ resources/views/store/profile/change-password.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     @extends('common.header')
     <div class="main-panel">
-        <div class="row">
-            <div class="col-md-10 offset-2">
+        <div class="content-wrapper">
+            {{-- <div class="col-md-10 offset-2">
                 <div class="panel panel-default">
 
                     <h2 style="padding-top: 20px;">パスワードを変更する</h2>
@@ -75,6 +75,72 @@
                             </div>
                         </form>
                     </div>
+                </div>
+            </div> --}}
+
+            <div class="row">
+                <div class="col-md-8 offset-md-2 grid-margin stretch-card">
+                <div class="card">
+                    @if (session('error'))
+                    <div class="alert alert-danger">
+                        {{ session('error') }}
+                    </div>
+                @endif
+                @if (session('success'))
+                    <div class="alert alert-success">
+                        {{ session('success') }}
+                    </div>
+                @endif
+                @if($errors)
+                    @foreach ($errors->all() as $error)
+
+                        <div class="alert alert-danger">
+                            @if ($errors->has('new-password'))
+                                @if (strpos($errors->first('new-password'), 'confirmation does not match')!== false)
+                                    新しいパスワードと確認パスワードが同じではありません
+                                @else
+                                    新しいパスワードは6文字以上である必要があります。
+                                @endif
+                            @elseif ($errors->has('current-password'))
+                                {{ $errors->first('current-password') }}
+                            @endif
+
+                        </div>
+
+                    @endforeach
+                @endif
+                    <div class="card-body">
+                    <h4 class="card-title">パスワードを変更する</h4>
+                    <form class="forms-sample" method="POST" action="{{ route('store.profile.changePassword') }}">
+                        @csrf
+                        <div class="form-group row">
+                            <label for="name" class="col-sm-3 col-form-label">現在のパスワード</label>
+                            <div class="col-sm-9">
+                                <input id="current-password" type="password" class="form-control" name="current-password" required>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="name" class="col-sm-3 col-form-label">新しいパスワード</label>
+                            <div class="col-sm-9">
+                                <input id="new-password" type="password" class="form-control" name="new-password" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="name" class="col-sm-3 col-form-label">新しいパスワードを確認</label>
+                            <div class="col-sm-9">
+                                <input id="new-password-confirm" type="password" class="form-control" name="new-password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group row text-center">
+                            <div class="col-sm-12 text-center">
+                                <button type="submit" class="btn btn-primary form-action-btn mr-2">変更</button>
+                            </div>
+                        </div>
+                    </form>
+                    </div>
+                </div>
                 </div>
             </div>
         </div>
